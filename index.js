3305785332f9f8460324d9c66a9d51627f242a5f
Sangsriti <sangsriti14.slg@gmail.com>
2025-08-18 03:42:09 +0530

    Stateful auth files added

--- index.js
+++ index.js
@@ -1,23 +1,33 @@
 const express = require("express");
 const path = require("path");
+const cookieParser = require("cookie-parser");
+const { connectToMongoDb } = require("./connection");
+
+const {restrictToLoggedInUserOnly, checkAuth} = require("./middlewares/auth");
+
+//Routes
 const urlRoute = require("./routes/url");
 const staticRoute = require("./routes/staticRouter");
-const { connectToMongoDb } = require("./connection");
-const PORT = 8001;
+const userRoute = require("./routes/user");
 
 const app = express();
+const PORT = 8001;
 
 connectToMongoDb("mongodb://127.0.0.1:27017/short-url");
+
+//Middleware plugins
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
+app.use(cookieParser());
 
 app.set("view engine", "ejs");
 app.set("views", path.resolve("./views"));
 
 
 
-app.use("/url",urlRoute);
-app.use("/", staticRoute);
+app.use("/url", restrictToLoggedInUserOnly ,urlRoute);
+app.use("/user",  userRoute);
+app.use("/", checkAuth, staticRoute);
 
 
 app.listen(PORT, ()=>console.log(`Server started at Port: ${PORT}`));
